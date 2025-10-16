5321934a5613392ea24764b8247b0bff6394742a
Louis Padé <louis.pade@etud.u-picardie.fr>
2025-10-16 11:51:59 +0200

    Cours du 16/10

---
+++ UniversiteDomain/UseCases/ParcoursUseCases/Create/CreateParcoursUseCase.cs
@@ -0,0 +1,39 @@
+﻿using UniversiteDomain.DataAdapters;
+using UniversiteDomain.Entities;
+using UniversiteDomain.Exceptions.EtudiantExceptions;
+using UniversiteDomain.Util;
+
+namespace UniversiteDomain.UseCases.ParcoursUseCases.Create;
+
+
+public class CreateParcoursUseCase(IParcoursRepository parcoursRepository)
+{
+    public async Task<Parcours> ExecuteAsync(string NomParcours, int AnneeFormation)
+    {
+        var parcours = new Parcours{NomParcours = NomParcours, AnneeFormation = AnneeFormation};
+        return await ExecuteAsync(parcours);
+    }
+    public async Task<Parcours> ExecuteAsync(Parcours parcours)
+    {
+        await CheckBusinessRules(parcours);
+        Parcours et = await parcoursRepository.CreateAsync(parcours);
+        parcoursRepository.SaveChangesAsync().Wait();
+        return et;
+    }
+    private async Task CheckBusinessRules(Parcours parcours)
+    {
+        ArgumentNullException.ThrowIfNull(parcours);
+        ArgumentNullException.ThrowIfNull(parcours.NomParcours);
+        ArgumentNullException.ThrowIfNull(parcoursRepository);
+        
+        // On recherche un parcours avec le même nom
+        List<Parcours> existe = await parcoursRepository.FindByConditionAsync(e=>e.NomParcours.Equals(parcours.NomParcours));
+    
+        // Si un parcours avec le même nom de parcours existe déjà, on lève une exception personnalisée
+        if (existe is {Count:>0}) throw new DuplicateNumEtudException(parcours.NomParcours+ " - ce nom de parcours est déjà affecté à un parcours");
+        
+        
+        ArgumentOutOfRangeException.ThrowIfNegative(parcours.AnneeFormation);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(parcours.AnneeFormation, DateTime.Today.Year);
+    }
+}
---
+++ UniversiteDomain/UseCases/ParcoursUseCases/EtudiantDansParcours/AddEtudiantDansParcours.cs
@@ -0,0 +1,49 @@
+﻿using UniversiteDomain.DataAdapters;
+using UniversiteDomain.Entities;
+using UniversiteDomain.Exceptions.EtudiantExceptions;
+using UniversiteDomain.Exceptions.ParcoursExceptions;
+
+namespace UniversiteDomain.UseCases.ParcoursUseCases.Add;
+
+public class AddEtudiantDansParcours(IEtudiantRepository etudiantRepository, IParcoursRepository parcoursRepository)
+{
+    public async Task<Parcours> ExecuteAsync(Etudiant etudiant, Parcours parcours)
+    {
+        ArgumentNullException.ThrowIfNull(etudiant);
+        ArgumentNullException.ThrowIfNull(parcours);
+        return await ExecuteAsync(etudiant.Id, parcours.Id);
+    }
+
+    public async Task<Parcours> ExecuteAsync(long idEtudiant, long idParcours)
+    {
+        await CheckBusinessRules(idEtudiant, idParcours);
+        return await parcoursRepository.AddEtudiantAsync(idParcours, idEtudiant);
+    }
+
+    
+    
+    private async Task CheckBusinessRules(long idParcours, long idEtudiant)
+    {
+        // Vérification des paramètres
+        ArgumentNullException.ThrowIfNull(idParcours);
+        ArgumentNullException.ThrowIfNull(idEtudiant);
+        
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(idParcours);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(idEtudiant);
+        
+        // Vérifions tout d'abord que nous sommes bien connectés aux datasources
+        ArgumentNullException.ThrowIfNull(etudiantRepository);
+        ArgumentNullException.ThrowIfNull(parcoursRepository);
+        
+        // On recherche l'étudiant
+        List<Etudiant> etudiant = await etudiantRepository.FindByConditionAsync(e=>e.Id.Equals(idEtudiant));;
+        if (etudiant is { Count: 0 }) throw new EtudiantNotFoundException(idEtudiant.ToString());
+        // On recherche le parcours
+        List<Parcours> parcours = await parcoursRepository.FindByConditionAsync(p=>p.Id.Equals(idParcours));;
+        if (parcours is { Count: 0 }) throw new ParcoursNotFoundException(idParcours.ToString());
+        
+        // On vérifie que l'étudiant n'est pas déjà dans le parcours
+        List<Etudiant> inscrit = await etudiantRepository.FindByConditionAsync(e=>e.Id.Equals(idEtudiant) && e.ParcoursSuivi.Id.Equals(idParcours));
+        if (inscrit is { Count: > 0 }) throw new DuplicateInscriptionException(idEtudiant+" est déjà inscrit dans le parcours dans le parcours : "+idParcours);      
+    }
+}       
